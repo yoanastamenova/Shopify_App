3e282952102bf1d6decd9f70574e1da02f74e9e0
yoanastamenova <yoana.stamenovaa@gmail.com>
2024-11-19 11:24:23 +0100

    feat: card component created and some features included

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
