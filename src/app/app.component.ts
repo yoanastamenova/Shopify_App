4b10d1ffdce6b0cdcbac7e0f23f53471c3f8be93
yoanastamenova <yoana.stamenovaa@gmail.com>
2024-11-19 10:07:36 +0100

    feat: added logo + icon and rendered all the components in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent,MainPageComponent,SideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
