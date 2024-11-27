f3af707e553955e966ad3b57335dff314717d141
yoanastamenova <yoana.stamenovaa@gmail.com>
2024-11-27 17:00:43 +0100

    refactor: add new view of Home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MainPageComponent } from './main-page/main-page.component';
-import { SideBarComponent } from './side-bar/side-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,MainPageComponent,SideBarComponent],
+  imports: [RouterOutlet, HeaderComponent, MainPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
